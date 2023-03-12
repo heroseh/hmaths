e4306581a2f2daf8dca252bd0f466302d1984920
Henry Rose <heroseh@protonmail.com>
2023-03-12 19:00:04 +0000

    renamed files and remove generated warning header in each file

---
+++ hmaths.h
@@ -0,0 +1,10 @@
+#ifndef _HMATHS_MATHS_H_
+#define _HMATHS_MATHS_H_
+
+#include "hmaths_types.h"
+#include "hmaths_scalar.h"
+#include "hmaths_vector.h"
+#include "hmaths_matrix.h"
+#include "hmaths_generics.h"
+
+#endif // _HMATHS_MATHS_H_
--- scripts/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p build
-clang -o build/generator tools/generator.c
-if test $? -ne 0; then
-	exit
-fi
-./build/generator
